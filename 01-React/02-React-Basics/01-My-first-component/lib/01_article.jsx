4e5b043209505c8085a8896c0bc9d47fbf95a2c2
roy yoo <royyoo123@gmail.com>
2021-03-17 21:51:15 -0700

    first react component

--- 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
+++ 01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
@@ -1,9 +1,15 @@
 // TODO: import the right dependencies
-
+import React from 'react';
 // TODO: code the Article component
-
+const Article = (props) => {
+	return <div className="article">
+					<h2>{props.title}</h2>
+					<p>{props.body}</p>
+					</div>
+}
 
 
 
 
 // TODO: export your Article component for tests
+export default Article;
